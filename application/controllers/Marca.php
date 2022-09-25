8518213f7e2aa9dadead35a9ca6166a6aebd1acf
jorgecm <jorg3cm@gmail.com>
2022-09-24 23:11:22 -0400

    Cambios en el controlador Marca

--- application/controllers/Marca.php
+++ application/controllers/Marca.php
@@ -3,12 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Marca extends CI_Controller {
 
-	public function index()
+        public function index()
 	{
+
                 if($this->session->userdata('tipo')=='admin')
                 {
-                        $lista=$this->motocicleta_model->listamarcas();
-                        $data['marca']=$lista;
+                        $lista=$this->marca_model->listamarcas();
+                        $data['sucursales']=$lista;
 
 
                         $this->load->view('inc/headersbadmin2');
@@ -17,30 +18,27 @@ class Marca extends CI_Controller {
                         $this->load->view('listamarca',$data);
                         $this->load->view('inc/creditossbadmin2');
                         $this->load->view('inc/footersbadmin2');
-                        /*$this->load->view('inc/header');
-                        $this->load->view('listamotocicleta',$data);
-                        $this->load->view('inc/footer');*/
-                        }
+
+                }
                 else
                 {
                         //El usuario no esta logueado
                         redirect('usuarios/panel','refresh');
 
-                } 
-		
+                } 	
 	}
 
         public function guest()
 	{
                 if($this->session->userdata('tipo')=='guest')
                 {
-                        $lista=$this->motocicleta_model->listamarcas();
-                        $data['marca']=$lista;
-                        
+                        $lista=$this->marca_model->listamarcas();
+                        $data['marcas']=$lista;
+
                         $this->load->view('inc/headersbadmin2');
                         $this->load->view('inc/sidebarsbadmin2guest');
                         $this->load->view('inc/topbarsbadmin2');
-                        $this->load->view('panelguestlistamarcas',$data);
+                        $this->load->view('panelguestlistasucursales',$data);
                         $this->load->view('inc/creditossbadmin2');
                         $this->load->view('inc/footersbadmin2');
                 }  
@@ -50,25 +48,25 @@ class Marca extends CI_Controller {
 	{
                 if($this->session->userdata('tipo')=='admin')
                 {
-                        $this->load->view('inc/headersbadmin2');
-                        $this->load->view('inc/sidebarsbadmin2');
-                        $this->load->view('inc/topbarsbadmin2');
-                        $this->load->view('formulariomarca');
-                        $this->load->view('inc/creditossbadmin2');
-                        $this->load->view('inc/footersbadmin2');  
-                } 
+                $this->load->view('inc/headersbadmin2');
+                $this->load->view('inc/sidebarsbadmin2');
+                $this->load->view('inc/topbarsbadmin2');
+                $this->load->view('formulariomarca');
+                $this->load->view('inc/creditossbadmin2');
+                $this->load->view('inc/footersbadmin2');
+                }
                 else
                 {
-                        $this->load->view('inc/headersbadmin2');
-                        $this->load->view('inc/sidebarsbadmin2guest');
-                        $this->load->view('inc/topbarsbadmin2');
-                        $this->load->view('formulariomarca');
-                        $this->load->view('inc/creditossbadmin2');
-                        $this->load->view('inc/footersbadmin2');      
-                }     
+                $this->load->view('inc/headersbadmin2');
+                $this->load->view('inc/sidebarsbadmin2guest');
+                $this->load->view('inc/topbarsbadmin2');
+                $this->load->view('formulariomarca');
+                $this->load->view('inc/creditossbadmin2');
+                $this->load->view('inc/footersbadmin2');
+                }        
                 /*$this->load->view('inc/header');
-                $this->load->view('formulariomoto');
-                $this->load->view('inc/footer');*/	
+                $this->load->view('formulario');
+                $this->load->view('inc/footer');	*/
 	}
 
         public function agregarbd()
@@ -79,37 +77,31 @@ class Marca extends CI_Controller {
                 redirect('marca/index','refresh');
 	}
 
-        public function eliminarbd()
-        {
-                $idmarca=$_POST['idmarca'];
-                $this->motocicleta_model->eliminarmotocicleta($idmarca);
-                redirect('marca/index','refresh');
-        }
-
         public function modificar()
         {
-                $idmarca=$_POST['idmarca'];
+                $idsucursal=$_POST['idmarca'];
                 $data['infomarca']=$this->marca_model->recuperarmarca($idmarca);
                 
                 $this->load->view('inc/headersbadmin2');
                 $this->load->view('inc/sidebarsbadmin2');
                 $this->load->view('inc/topbarsbadmin2');
-                $this->load->view('formulariomarcamodificar',$data);
+                $this->load->view('formulariomodificarmarca',$data);
                 $this->load->view('inc/creditossbadmin2');
                 $this->load->view('inc/footersbadmin2');
                 /*$this->load->view('inc/header');
-                $this->load->view('formulariomotomodificar',$data);
+                $this->load->view('formulariomodificar',$data);
                 $this->load->view('inc/footer');*/
         }
 
         public function modificarbd()
         {
                 $idmarca=$_POST['idmarca'];
-                $data['nombreMarca']=$_POST['nombreMarca'];
+                $data['nombreMarca']=$_POST['nombremarca'];
                 $data['fechaActualizacion']=date('Y-m-d H:i:s');
+
                 
                 $this->marca_model->modificarmarca($idmarca,$data);
-                redirect('motocicleta/index','refresh');
+                redirect('marca/index','refresh');
         }
 
         public function deshabilitarbd()
@@ -124,16 +116,16 @@ class Marca extends CI_Controller {
         public function deshabilitados()
         {
                 $lista=$this->marca_model->listamarcasdeshabilitados();
-                $data['marca']=$lista;
+                $data['marcas']=$lista;
 
                 $this->load->view('inc/headersbadmin2');
                 $this->load->view('inc/sidebarsbadmin2');
                 $this->load->view('inc/topbarsbadmin2');
-                $this->load->view('listamarcadeshabilitados',$data);
+                $this->load->view('listamarcadeshabilitado',$data);
                 $this->load->view('inc/creditossbadmin2');
                 $this->load->view('inc/footersbadmin2');
                 /*$this->load->view('inc/header');
-                $this->load->view('listamotodeshabilitados',$data);////////
+                $this->load->view('listadeshabilitados',$data);
                 $this->load->view('inc/footer');*/
         }
 
@@ -145,6 +137,5 @@ class Marca extends CI_Controller {
                 $this->marca_model->modificarmarca($idmarca,$data);
                 redirect('marca/deshabilitados','refresh');
         }
-       
 
 }
